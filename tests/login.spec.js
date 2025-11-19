05b02631667a84be6c405201dc31b54ff13eaa8b
Gholami-Bahareh <baharehgholamii86@gmail.com>
2025-11-19 22:54:07 +0100

    tests

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -12,3 +12,20 @@ test('UI element should be visible', async ({ page }) => {
     await expect(loginPage.forgotPasswordLink).toBeVisible();
     await expect(loginPage.registerButton).toBeVisible();
 });
+
+test('Succesful Login', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/login');
+
+    await loginPage.login('validemail@hotmail.com','123456');
+    await expect(page.locator('a[href="/logout"]')).toBeVisible();
+    
+});
+
+test('Unsuccesful Login', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await page.goto('/login');
+
+    await loginPage.login('invalidemail@hotmail.com','123456');
+    await expect(loginPage.errorMessage).toBeVisible();
+});
