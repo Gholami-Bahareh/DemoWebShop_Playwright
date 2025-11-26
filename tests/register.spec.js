90c4c19480d96a478afd34c48c8af402c585f9a1
Gholami-Bahareh <baharehgholamii86@gmail.com>
2025-11-26 10:52:04 +0100

    register test continue

--- tests/register.spec.js
+++ tests/register.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 const { RegisterPage } = require('../page-objects/RegisterPage');
 const { randomEmail , randomPassword  } = require('../utils/helpers');
 
-test.only('Successful Registration', async ({ page }) => {
+test.only ('Successful Registration', async ({ page }) => {
     const registerPage = new RegisterPage(page);
     
     const email = randomEmail(); 
@@ -11,6 +11,21 @@ test.only('Successful Registration', async ({ page }) => {
 
     await registerPage.register('John', 'Doe', email  , password);
     await expect(page.locator('div[class=result]')).toHaveText('Your registration completed');
+    await expect(page).toHaveURL(/.*registerresult/);
 
+});
+
+test ('UI element should be visible', async ({ page }) => {
+    const registerPage = new RegisterPage(page);
+    await page.goto('/register');
+
+    await expect(registerPage.genderMaleRadio).toBeVisible();
+    await expect(registerPage.genderFemaleRadio).toBeVisible();
+    await expect(registerPage.firstnameInput).toBeVisible();
+    await expect(registerPage.lastnameInput).toBeVisible();
+    await expect(registerPage.emailInput).toBeVisible();
+    await expect(registerPage.passwordInput).toBeVisible();
+    await expect(registerPage.confirmPasswordInput).toBeVisible();
+    await expect(registerPage.registerButton).toBeVisible();
 
 });
