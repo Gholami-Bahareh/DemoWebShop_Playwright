4d5a8e86c0c473a4982e0fa5f4016f169f9d1f9a
Gholami-Bahareh <baharehgholamii86@gmail.com>
2025-12-03 14:22:22 +0100

    product page begin

--- page-objects/ProductPage.js
+++ page-objects/ProductPage.js
@@ -3,10 +3,27 @@ class ProductPage {
         this.page = page;
 
     //Locators
+    this.categoryMenu = page.locator('.block.block-category-navigation');
+    this.categoryItems = page.locator('.block.block-category-navigation li a');
+    this.subCategoryItems = page.locator('.block.block-category-navigation li.active li a');
+    this.productItems = page.locator('.product-grid .item-box');
+    this.productPicture = page.locator('.product-grid .picture');
+    this.productTile = page.locator('.product-grid .details .product-title a');
+    this.productRating = page.locator('.product-grid .product-rating-box');
+    this.productPrice = page.locator('.product-grid .prices');
 
     }
     
     //Methods / Functions
+    async goto(){
+        await this.page.goto();
+    }
+
+    async openFirstCategory() {
+        await this.categoryItems.first().click();
+    }
+
+    
 
     }
 
--- tests/product.spec.js
+++ tests/product.spec.js
@@ -3,5 +3,8 @@ const { ProductPage } = require('../page-objects/ProductPage');
 
 
 test('UI element should be visible', async ({ page }) => {
+    const productPage = new ProductPage(page);
+    await productPage.goto();
+
     
 });
