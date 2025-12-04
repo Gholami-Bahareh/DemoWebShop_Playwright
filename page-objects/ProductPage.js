4677db23fb0f0560efd27760fe9f0214e0bb4895
Gholami-Bahareh <baharehgholamii86@gmail.com>
2025-12-04 09:50:24 +0100

    product page tests

--- page-objects/ProductPage.js
+++ page-objects/ProductPage.js
@@ -1,3 +1,5 @@
+const { expect } = require("allure-playwright");
+
 class ProductPage {
     constructor(page) {
         this.page = page;
@@ -7,24 +9,38 @@ class ProductPage {
     this.categoryItems = page.locator('.block.block-category-navigation li a');
     this.subCategoryItems = page.locator('.block.block-category-navigation li.active li a');
     this.productItems = page.locator('.product-grid .item-box');
-    this.productPicture = page.locator('.product-grid .picture');
-    this.productTile = page.locator('.product-grid .details .product-title a');
-    this.productRating = page.locator('.product-grid .product-rating-box');
-    this.productPrice = page.locator('.product-grid .prices');
+    // this.productPicture = page.locator('.product-grid .picture');
+    // this.productTile = page.locator('.product-grid .details .product-title a');
+    // this.productRating = page.locator('.product-grid .product-rating-box');
+    // this.productPrice = page.locator('.product-grid .prices');
 
     }
     
     //Methods / Functions
     async goto(){
-        await this.page.goto();
+        await this.page.goto('/');
     }
 
     async openFirstCategory() {
         await this.categoryItems.first().click();
     }
 
-    
+     async hasProducts() {
+        return await this.productItems.count() > 0;
+    }
 
+    async openFirstSubCategory() {
+        await this.subCategoryItems.first().click();
+    }
+
+    async validateFirstProduct() {
+        const first = this.productItems.first();
+        await expect(first).toBeVisible();
+        await expect(first.locator('.picture')).toBeVisible();
+        await expect(first.locator('.details .product-title a')).toBeVisible();
+        await expect(first.locator('.product-rating-box')).toBeVisible();
+        await expect(first.locator('.prices')).toBeVisible();  
+    }
     }
 
 module.exports = { ProductPage };
--- tests/product.spec.js
+++ tests/product.spec.js
@@ -2,9 +2,22 @@ const { test, expect } = require('@playwright/test');
 const { ProductPage } = require('../page-objects/ProductPage');
 
 
-test('UI element should be visible', async ({ page }) => {
+test.only('UI element should be visible', async ({ page }) => {
     const productPage = new ProductPage(page);
     await productPage.goto();
+
+    await productPage.openFirstCategory();
+    if (await productPage.hasProducts()){
+
+        await expect(productPage.productItems).not.toHaveCount(0);
+        await productPage.validateFirstProduct();
+    }
+    else{
+        await productPage.openFirstSubCategory();
+        await expect(productPage.productItems).not.toHaveCount(0);
+        await productPage.validateFirstProduct();
+
+    }
 
     
 });
